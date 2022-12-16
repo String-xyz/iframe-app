840a218e6f4fe2136387fdf72d951d2db5e14b2d
Frostbourne <85953565+frostbournesb@users.noreply.github.com>
2022-12-16 12:11:44 -0500

    remove excess whitespace

--- src/lib/modals/onboarding/VerifyEmailForm.svelte
+++ src/lib/modals/onboarding/VerifyEmailForm.svelte
@@ -7,29 +7,27 @@
 	import Onboarding from './Onboarding.svelte';
 
 	import { email, modalManager } from '$lib/stores';
-	import { z } from "zod";
+	import { z } from 'zod';
 
 	let tosAgreement = false;
-	let firstNameInput = "";
-	let lastNameInput = ""
-	let emailInput = "";
+	let firstNameInput = '';
+	let lastNameInput = '';
+	let emailInput = '';
 
 	let isFNValid = true;
 	let isLNValid = true;
 	let isEmailValid = true;
 	let isTOSValid = true;
 
-
 	const emailSchema = z.string().trim().email();
 	const nameSchema = z.string().min(1);
-	
+
 	const handleVerify = () => {
 		if (!isValidInput()) return;
-		email.set(emailInput)
-		next()
+		email.set(emailInput);
+		next();
+	};
 
-	}
-	
 	const isValidInput = () => {
 		isEmailValid = emailSchema.safeParse(emailInput).success;
 
@@ -40,7 +38,7 @@
 		isTOSValid = tosAgreement;
 
 		return isEmailValid && isFNValid && isLNValid && isTOSValid;
-	}
+	};
 
 	//TODO: We want the error to be removed when the field is empty
 	// const checkEmailEmpty = () => {
@@ -60,7 +58,9 @@
 
 <ModalBase title="Verify your email" size="size-form">
 	<form on:submit|preventDefault={handleVerify}>
-		<p class="text-xl mt-5">To proceed, we'll need a bit of information and to verify your email.</p>
+		<p class="text-xl mt-5">
+			To proceed, we'll need a bit of information and to verify your email.
+		</p>
 		<div class="mt-5">
 			<div class="flex justify-between">
 				<div class="mt-4">
@@ -69,7 +69,7 @@
 						<input
 							bind:value={firstNameInput}
 							class="input input-bordered border-2 w-64"
-							class:border-error="{!isFNValid}"
+							class:border-error={!isFNValid}
 							placeholder="First name"
 							autofocus
 							required
@@ -82,7 +82,7 @@
 						<input
 							bind:value={lastNameInput}
 							class="input input-bordered border-2 w-64"
-							class:border-error="{!isLNValid}"
+							class:border-error={!isLNValid}
 							placeholder="Last name"
 							required
 						/>
@@ -95,7 +95,7 @@
 					<input
 						bind:value={emailInput}
 						class="input input-bordered border-2 w-full"
-						class:border-error="{!isEmailValid}"
+						class:border-error={!isEmailValid}
 						placeholder="test@string.xyz"
 						required
 					/>
@@ -106,11 +106,27 @@
 				</div>
 			</div>
 			<div class="flex justify-start mt-9">
-				<input type="checkbox" bind:checked={tosAgreement} class="checkbox checkbox-primary" class:border-error='{!isTOSValid}' />
-				<span class="ml-2 text-sm">I accept the 
-					<a href="https://www.string.xyz/terms-of-service" target='_blank' rel="noopener noreferrer" class="link link-primary">Terms of Service</a> 
-					and the 
-					<a href="https://www.string.xyz/privacy-policy" target='_blank' rel="noopener noreferrer" class="link link-primary">Privacy Policy</a>
+				<input
+					type="checkbox"
+					bind:checked={tosAgreement}
+					class="checkbox checkbox-primary"
+					class:border-error={!isTOSValid}
+				/>
+				<span class="ml-2 text-sm"
+					>I accept the
+					<a
+						href="https://www.string.xyz/terms-of-service"
+						target="_blank"
+						rel="noopener noreferrer"
+						class="link link-primary">Terms of Service</a
+					>
+					and the
+					<a
+						href="https://www.string.xyz/privacy-policy"
+						target="_blank"
+						rel="noopener noreferrer"
+						class="link link-primary">Privacy Policy</a
+					>
 				</span>
 			</div>
 			<div class="mt-7 float-right">
