12678d06256908d40dff29a438edc77ace52ef8a
frostbournesb <85953565+frostbournesb@users.noreply.github.com>
2023-02-04 05:55:40 -0500

    Put close button over title

--- src/lib/modals/checkout/ModalBase.svelte
+++ src/lib/modals/checkout/ModalBase.svelte
@@ -11,27 +11,22 @@
 </script>
 
 <div class="str-modal text-neutral border border-neutral">
-	<header class="flex justify-between">
-		<span class="text-2xl font-bold title">{title}</span>
-		<button on:click={close}><img src="/assets/close.svg" alt="Close" /></button>
+	<header class="flex flex-col justify-between">
+		<button class="ml-auto pr-6" on:click={close}><img src="/assets/close.svg" alt="Close" /></button>
+		<span class="text-2xl font-bold title pr-9 mt-1">{title}</span>
 	</header>
-	<div class="content">
+	<div class="content pr-9">
 		<slot />
 	</div>
-	<footer class="footer footer-center my-6 select-none text-xs">
-		<div class="inline">
-			<span class=""
-				>Powered by <img class="inline" src="/assets/string_text_logo.svg" alt="String" /></span
-			>
-		</div>
+	<footer class="flex flex-col justify-center items-center my-6 pr-9 text-xs">
+		<span class="">Powered by <img class="inline" src="/assets/string_text_logo.svg" alt="String" /></span>
 	</footer>
 </div>
 
 <style>
 	.str-modal {
 		padding-left: 36px;
-		padding-right: 36px;
-		padding-top: 36px;
+		padding-top: 24px;
 		border-radius: 8px;
 		max-width: 374px;
 		overflow-y: hidden;
